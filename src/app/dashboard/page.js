64d5adba5547a9eb5ea1d4d9b4a694a675e2c22f
adilraja11 <adilraja19@students.undip.ac.id>
2024-02-17 11:29:37 +0700

    fix project architecture

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,15 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="mx-10 my-5 space-y-5">
+    <div>
         <h1>Garlic bread with cheese: What the science tells us</h1>
         <p>
           For years parents have espoused the health benefits of eating garlic bread with cheese to their
           children, with the food earning such an iconic status in our culture that kids will often dress
           up as warm, cheesy loaf for Halloween.
         </p>
-        <button className="btn btn-active">Button</button>
+        <button className="btn btn-active">Default</button>
     </div>
   );
 }
