f5081a67869e9bdcb2f1ca5e7a87b93c2ac8d0cc
adilraja11 <adilraja19@students.undip.ac.id>
2024-02-17 14:47:20 +0700

    add page.js on empty directory

---
+++ src/app/(auth)/register/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+        <h1>Garlic bread with cheese: What the science tells us</h1>
+        <p>
+          For years parents have espoused the health benefits of eating garlic bread with cheese to their
+          children, with the food earning such an iconic status in our culture that kids will often dress
+          up as warm, cheesy loaf for Halloween.
+        </p>
+        <button className="btn btn-active">Default</button>
+    </div>
+  );
+}
