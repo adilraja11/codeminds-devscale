8d10981f95b911368777ac2b8f47b937cd562f61
adilraja11 <adilraja19@students.undip.ac.id>
2024-02-18 08:21:03 +0700

    add new router for login and register page

--- src/components/guest/AppBar.jsx
+++ src/components/guest/AppBar.jsx
@@ -1,14 +1,19 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 export const AppBar = () => {
+  const router = useRouter();
+
   return (
     <div className="navbar bg-base-300 px-3 py-4">
         <div className="flex-1">
             <a className="btn btn-ghost text-xl">Eventmakers</a>
         </div>
         <div className="navbar-end space-x-3">
-            <button className="btn w-32 btn-circle">Log In</button>
-            <button className="btn btn-neutral w-32 btn-circle">Register</button>
+            <button onClick={() => router.push("/auth/login")} className="btn w-32 btn-circle">Log In</button>
+            <button onClick={() => router.push("/auth/login")} className="btn btn-neutral w-32 btn-circle">Register</button>
         </div>
     </div>
   )
